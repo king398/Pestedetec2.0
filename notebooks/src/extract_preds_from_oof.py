613ef2c9cd0db325c6a4f8fc2d84757577e7241c
king398 <mithilsalunkhe@outlook.com>
2022-10-31 15:10:57 +0530

    make oof

--- notebooks/src/extract_preds_from_oof.py
+++ notebooks/src/extract_preds_from_oof.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import os
+import numpy as np
+from statistics import mean
+
 train_df = pd.read_csv('/home/mithil/PycharmProjects/PestDetect/data/Train.csv')
+train_labels_df = pd.read_csv('/home/mithil/PycharmProjects/Pestedetec2.0/data/train_modified.csv')
 ids = []
 labels = []
-pred_labels_path = '/home/mithil/PycharmProjects/PestDetect/pred_labels/yolo7x_different_aug_1600_image_size'
+pred_labels_path = '/home/mithil/PycharmProjects/Pestedetec2.0/oof_raw_preds/yolov5s6_image_size_1024_oof'
+id_label_dict = dict(zip(train_labels_df['image_id'].values, train_labels_df['number_of_worms'].values))
 
 
 def make_labels(id):
     id = id.split('.')[0]
-    ids.extend([f"{id}_pbw", f"{id}_abw"])
+    ids.extend([f"{id}_pbw.jpg", f"{id}_abw.jpg"])
     pbw = 0
     abw = 0
     if os.path.exists(
@@ -24,8 +29,27 @@ def make_labels(id):
     labels.extend([pbw, abw])
 
 
+def mae(y_true, y_pred):
+    return np.mean(np.abs(y_true - y_pred))
+
+
 list(map(make_labels, train_df['image_id_worm'].values))
 oof = pd.DataFrame({'image_id_worm': ids, 'label': labels}, index=None)
 oof.to_csv(
-    '/home/mithil/PycharmProjects/PestDetect/submission/yolo7x_different_aug_1600_image_size.csv',
+    '/home/mithil/PycharmProjects/Pestedetec2.0/oof_df/yolov5s6_image_size_1024_oof.csv',
     index=False)
+pred_label_dict = dict(zip(oof['image_id_worm'].values, oof['label'].values))
+
+
+def return_error(id):
+    id = id.split('.')[0]
+    pbw_label = id_label_dict[f'{id}_pbw.jpg']
+    abw_label = id_label_dict[f'{id}_abw.jpg']
+    pbw_pred = pred_label_dict[f'{id}_pbw.jpg']
+    abw_pred = pred_label_dict[f'{id}_abw.jpg']
+    error = mae(np.array(pbw_label), np.array(pbw_pred)) + mae(np.array(abw_label), np.array(abw_pred))
+    return error
+
+
+error = list(map(return_error, train_df['image_id_worm'].values))
+print(mean(error))
--- notebooks/src/make_oof_pred.py
+++ notebooks/src/make_oof_pred.py
@@ -4,11 +4,11 @@ import shutil
 
 for i in range(5):
     os.system(
-        f"python detect.py --weights /home/mithil/PycharmProjects/Pestedetec2.0/models/yolov5s6_image_size_1024/yolov5s6_image_size_1024_fold_{i}/weights/best.pt --half --nosave --img-size 1024 --source dataset/fold_{i}/images/val --name yolov5s6_image_size_1024_fold_{i}_val --save-txt")
+        f"python detect.py --weights /home/mithil/PycharmProjects/Pestedetec2.0/models/yolov5s6_image_size_1024/yolov5s6_image_size_1024_fold_{i}/weights/best.pt --half  --img-size 1024 --source dataset/fold_{i}/images/val --name yolov5s6_image_size_1024_fold_{i}_val --save-txt")
 preds_txt = glob.glob(
     'runs/detect/yolov5s6_image_size_1024_fold_*_val/labels/*.txt')
-os.makedirs('/home/mithil/PycharmProjects/Pestedetec2.0/oof/yolov5s6_image_size_1024_oof/', exist_ok=True)
+os.makedirs('/oof_raw_preds/yolov5s6_image_size_1024_oof/', exist_ok=True)
 for i in preds_txt:
-    shutil.copy(i, '/home/mithil/PycharmProjects/Pestedetec2.0/oof/yolov5s6_image_size_1024_oof/')
+    shutil.copy(i, '/oof_raw_preds/yolov5s6_image_size_1024_oof/')
     os.remove(i)
-os.system('rm -r runs/detect/')
+#os.system('rm -r runs/detect/')
